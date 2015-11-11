d9743a8b55391049fd39350da8af36ebd706c1c1
mgechev <mgechev@gmail.com>
2015-11-11 14:19:50 +0200

    Add basic demo for configuring providers

---
+++ app/ch5/ts/configuring-providers/sample.ts
@@ -0,0 +1,26 @@
+import 'reflect-metadata';
+import {
+  Injector, Inject, Injectable,
+  OpaqueToken, forwardRef, provide
+} from 'angular2/angular2';
+
+const BUFFER_SIZE = new OpaqueToken('buffer-size');
+
+class Buffer {
+  constructor(@Inject(BUFFER_SIZE) private size:Number) {
+    console.log(this.size);
+  }
+}
+
+@Injectable()
+class Socket {
+  constructor(private buffer: Buffer) {}
+}
+
+let injector = Injector.resolveAndCreate([
+  provide(BUFFER_SIZE, { useValue: 42 }),
+  provide(Buffer, { useClass: Buffer } ),
+  provide(Socket, { useClass: Socket } )
+]);
+
+injector.get(Socket);
