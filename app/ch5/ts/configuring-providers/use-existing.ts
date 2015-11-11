9cbc225a8d89a31058ca23a0a92180e2e414d635
mgechev <mgechev@gmail.com>
2015-11-11 15:44:11 +0200

    Update use existing

--- app/ch5/ts/configuring-providers/use-existing.ts
+++ app/ch5/ts/configuring-providers/use-existing.ts
@@ -12,12 +12,19 @@ class UserService {
   constructor(public http: Http) {}
 }
 
+// let injector = Injector.resolveAndCreate([
+//   DummyService,
+//   provide(Http, { useExisting: DummyService }),
+//   UserService
+// ]);
+
+// let us:UserService = injector.get(UserService);
+
+// console.log(us.http instanceof DummyService);
+
 let injector = Injector.resolveAndCreate([
-  DummyService,
-  provide(Http, { useClass: DummyService }),
+  provide(DummyService, { useValue: 42 }),
+  provide(Http, { useExisting: DummyService }),
   UserService
 ]);
-
-let us:UserService = injector.get(UserService);
-
-console.log(us.http instanceof DummyService);
+console.assert(injector.get(UserService).http === 42);
