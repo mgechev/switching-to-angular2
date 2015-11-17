dcb9f4bcca9604d55fb153e3d1193f772c984a0a
mgechev <mgechev@gmail.com>
2015-11-17 10:30:07 +0200

    Update examples

--- app/ch5/es5/simple-example/app.js
+++ app/ch5/es5/simple-example/app.js
@@ -9,29 +9,28 @@ var MarkdownPane = ng.Component({
   selector: 'markdown-pane',
   viewProviders: [Markdown],
   styles: [
-    `.pane {
-      width: auto;
-      display: inline-block;
-      border: 1px solid black;
-    }
-    .pane-title {
-      border-bottom: 1px solid black;
-      background-color: #eee;
-    }
-    .pane-content,
-    .pane-title {
-      padding: 5px;
-    }`
+    '.pane {' +
+      'width: auto;' +
+      'display: inline-block;' +
+      'border: 1px solid black;' +
+    '}' +
+    '.pane-title {' +
+      'border-bottom: 1px solid black;' +
+      'background-color: #eee;' +
+    '}' +
+    '.pane-content,' +
+    '.pane-title {' +
+    '  padding: 5px;' +
+    '}'
   ],
-  template: `
-    <div class="pane">
-      <div class="pane-title">
-        <ng-content select="pane-title"></ng-content>
-      </div>
-      <div class="pane-content">
-        <ng-content select="pane-content"></ng-content>
-      </div>
-    </div>`
+  template: '<div class="pane">' +
+      '<div class="pane-title">' +
+        '<ng-content select="pane-title"></ng-content>' +
+      '</div>' +
+      '<div class="pane-content">' +
+        '<ng-content select="pane-content"></ng-content>' +
+      '</div>' +
+    '</div>'
 })
 .Class({
   constructor: [ng.Inject(Markdown), ng.Inject(ng.ElementRef), function (md, el) {
@@ -45,7 +44,7 @@ var MarkdownPane = ng.Component({
     title.innerHTML = this.md.toHTML(title.innerHTML);
     content.innerHTML = this.md.toHTML(content.innerHTML);
   }
-})
+});
 
 var App = ng.Component({
   selector: 'app',
