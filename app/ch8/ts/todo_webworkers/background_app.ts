4cc3e268a26b7233857c58ea7146b52cb75ad31c
mgechev <mgechev@gmail.com>
2016-03-03 20:43:46 +0200

    refactor: remove useless ref

--- app/ch8/ts/todo_webworkers/background_app.ts
+++ app/ch8/ts/todo_webworkers/background_app.ts
@@ -1,8 +1,6 @@
-import {platform} from 'angular2/core';
 import {WORKER_APP_PLATFORM, WORKER_APP_APPLICATION} from 'angular2/platform/worker_app';
 
-import {Component, Input, Output, EventEmitter} from 'angular2/core';
-import {NgModel} from 'angular2/common';
+import {platform, Component, Input, Output, EventEmitter} from 'angular2/core';
 
 interface Todo {
   completed: boolean;
