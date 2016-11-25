1f1b390496c1c47e52780bf18c818b5968318d3d
mgechev <mgechev@gmail.com>
2016-11-24 16:00:02 -0800

    refactor(ch8): migrate to new webworkers and ngmodules

--- tools/config.ts
+++ tools/config.ts
@@ -77,6 +77,8 @@ const SYSTEM_CONFIG_DEV = {
     '@angular/platform-browser': '/node_modules/@angular/platform-browser/bundles/platform-browser.umd.js',
     '@angular/platform-browser-dynamic': '/node_modules/@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js',
     '@angular/router': '/node_modules/@angular/router/bundles/router.umd.js',
+    '@angular/platform-webworker': '/node_modules/@angular/platform-webworker/bundles/platform-webworker.umd.js',
+    '@angular/platform-webworker-dynamic': '/node_modules/@angular/platform-webworker-dynamic/bundles/platform-webworker-dynamic.umd.js',
 
     '@angular/common/testing': '/node_modules/@angular/common/bundles/common-testing.umd.js',
     '@angular/compiler/testing': '/node_modules/@angular/compiler/bundles/compiler-testing.umd.js',
