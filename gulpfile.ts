16cb04c2c8e7de155a587c301872766c796d0482
mgechev <mgechev@gmail.com>
2017-01-03 12:38:05 +0200

    chore: trim the gulpfile tasks

--- gulpfile.ts
+++ gulpfile.ts
@@ -5,7 +5,6 @@ import {runSequence, task} from './tools/utils';
 // Clean (override).
 gulp.task('clean',       task('clean', 'all'));
 gulp.task('clean.dist',  task('clean', 'dist'));
-gulp.task('clean.test',  task('clean', 'test'));
 gulp.task('clean.tmp',   task('clean', 'tmp'));
 
 gulp.task('check.versions', task('check.versions'));
@@ -27,40 +26,12 @@ gulp.task('build.dev', done =>
               done));
 
 // --------------
-// Build prod.
-gulp.task('build.prod', done =>
-  runSequence('clean.dist',
-              'clean.tmp',
-              'build.assets.dev',
-              'build.html_css.prod',
-              'build.deps',
-              'build.js.prod',
-              'build.bundles',
-              'build.index',
-              done));
-
-// --------------
 // Watch.
 gulp.task('build.dev.watch', done =>
   runSequence('build.dev',
               'watch.dev',
               done));
 
-gulp.task('build.test.watch', done =>
-  runSequence('build.test',
-              'watch.test',
-              done));
-
-// --------------
-// Test.
-gulp.task('test', done =>
-  runSequence('clean.test',
-              'tslint',
-              'build.test',
-              'karma.start',
-              done));
-
-// --------------
 // Serve.
 gulp.task('serve', done =>
   runSequence('build.dev',
@@ -68,10 +39,3 @@ gulp.task('serve', done =>
               'watch.serve',
               done));
 
-// --------------
-// Docs
-// Disabled until https://github.com/sebastian-lenz/typedoc/issues/162 gets resolved
-// gulp.task('docs', done =>
-//   runSequence('build.docs',
-//               'serve.docs',
-//               done));
